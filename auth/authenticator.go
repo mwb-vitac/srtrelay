e1c9d69e878fe501d3609a8fa1aa2d3889fc406a
Michael Bruns <michael.bruns@vitac.com>
2025-02-05 15:28:35 -0800

    Add staticsecure Authenticator which gets a passphrase for the streamid
    The SRT callback function adds the passphrase to the SRT connection

--- auth/authenticator.go
+++ auth/authenticator.go
@@ -5,3 +5,7 @@ import "github.com/voc/srtrelay/stream"
 type Authenticator interface {
 	Authenticate(stream.StreamID) bool
 }
+
+type AuthSecure interface {
+	GetPassphrase(stream.StreamID) string
+}
